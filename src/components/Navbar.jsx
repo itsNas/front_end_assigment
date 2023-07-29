e1892447517d7481243e9b1944e70b2dfb40670e
nasrul-o7 <nasrullah.saloji@gmail.com>
2023-07-29 23:46:33 +0800

    navbar components

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,22 +1,38 @@
 import { BsArrowLeft } from "react-icons/bs";
-import { Link } from "react-router-dom";
+import { NavLink, useLocation } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { navbarItems } from "../constant";
 
 function Navbar() {
+  const location = useLocation();
+  const [selectedPage, setSelectedPage] = useState(location.pathname);
+
+  useEffect(() => {
+    // Set the "Home" page as the initially selected page on first loading
+    setSelectedPage(location.pathname);
+  }, [location.pathname]);
+
   return (
-    <div>
-      <Link to="/">
-        <button>
+    <div className="mx-auto p-6 max-w-md">
+      <NavLink to="/">
+        <button className="text-rose-500 text-2xl mb-4">
           <BsArrowLeft />
         </button>
-      </Link>
-      <h1>Morning Masscoder</h1>
-      <ul>
-        <li>
-          <Link to="/profile">Profile</Link>
-        </li>
-        <li>
-          <Link to="/leaves">Leaves</Link>
-        </li>
+      </NavLink>
+      <h1 className="text-3xl mb-4">Morning Masscoder</h1>
+      <ul className="flex justify-between border-b-2 py-4 first:pt-0 last:pb-0">
+        {navbarItems.map((page) => (
+          <li key={page.path} onClick={() => setSelectedPage(page.path)}>
+            <NavLink to={page.path} className="relative group px-4 py-2">
+              <span className="relative z-10 text-lg">{page.label}</span>
+              <span
+                className={`absolute bottom-0 left-0 w-full h-1 bg-zinc-700 ${
+                  selectedPage === page.path ? "scale-x-100" : "scale-x-0"
+                } transition-transform`}
+              ></span>
+            </NavLink>
+          </li>
+        ))}
       </ul>
     </div>
   );
---
+++ src/constant/index.js
@@ -0,0 +1,5 @@
+export const navbarItems = [
+  { path: "/", label: "Home" },
+  { path: "/profile", label: "Profile" },
+  { path: "/leaves", label: "Leave Balance" },
+];
