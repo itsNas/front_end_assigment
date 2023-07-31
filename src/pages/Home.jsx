d753d725aaf68b14fae64ab9d1304385889fdeed
nasrul-o7 <nasrullah.saloji@gmail.com>
2023-07-31 14:04:02 +0800

    update style on dummy page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,11 @@
 function Home() {
   return (
-    <div className="h-screen max-w-screen p-6 flex justify-center mt-20 text-center">
-      This is just a dummy home page. Click on the Leave balance link to see the
-      main assignment
+    <div className="h-screen max-w-md mx-auto p-6 flex justify-center mt-20 text-center">
+      <p>
+        {" "}
+        This is just a dummy home page. Click on the Leave balance link to see
+        the main assignment
+      </p>
     </div>
   );
 }
